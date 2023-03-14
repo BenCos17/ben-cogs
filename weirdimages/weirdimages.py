7cba3caf0d9b39a7a746f6c9fdda052643e14afa
Ben Cos <52817096+BenCos17@users.noreply.github.com>
2023-03-14 19:23:48 +0000

    more stuff

--- weirdimages/weirdimages.py
+++ weirdimages/weirdimages.py
@@ -2,16 +2,22 @@ import discord
 from redbot.core import commands
 from PIL import Image, ImageDraw, ImageFilter
 import random
+import asyncio
 
 class WeirdImages(commands.Cog):
     def __init__(self, bot):
         self.bot = bot
+        self.cooldowns = commands.CooldownMapping.from_cooldown(1, 15, commands.BucketType.user)
 
     @commands.command()
+    @commands.cooldown(1, 15, commands.BucketType.user)
     async def weird(self, ctx, size:int=500):
         """
-        Generate a weird image
+        Generate a weird image with random shapes and filters applied
         """
+        if self.cooldowns.get_bucket(ctx.message).update_rate_limit():
+            return await ctx.send("You're on cooldown. Please wait before running this command again.")
+        
         # create a blank image
         img = Image.new('RGB', (size, size), (random.randint(0, 255), random.randint(0, 255), random.randint(0, 255)))
         # get a drawing context
@@ -29,8 +35,19 @@ class WeirdImages(commands.Cog):
             draw.polygon([(x1, y1), (x2, y2), (random.randint(0, size), random.randint(0, size))], outline=(random.randint(0, 255), random.randint(0, 255), random.randint(0, 255)), width=3)
 
         # apply some filters
-        img = img.filter(ImageFilter.CONTOUR)
-        img = img.filter(ImageFilter.SMOOTH_MORE)
+        filters = [
+            ImageFilter.BLUR, 
+            ImageFilter.CONTOUR, 
+            ImageFilter.DETAIL, 
+            ImageFilter.EDGE_ENHANCE, 
+            ImageFilter.EDGE_ENHANCE_MORE, 
+            ImageFilter.EMBOSS, 
+            ImageFilter.FIND_EDGES, 
+            ImageFilter.SHARPEN, 
+            ImageFilter.SMOOTH, 
+            ImageFilter.SMOOTH_MORE
+        ]
+        img = apply_random_filters(img, filters)
 
         # save the image to a file
         img.save('weird.png')
@@ -38,5 +55,5 @@ class WeirdImages(commands.Cog):
         # send the image to the chat
         await ctx.send(file=discord.File('weird.png'))
 
-def setup(bot):
-    bot.add_cog(WeirdImages(bot))
+    @commands.command()
+    @commands.cooldown(1
