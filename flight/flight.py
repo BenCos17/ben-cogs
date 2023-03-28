b131022bf4bcead486e3aa6f8f358b413f55a848
Ben Cos <52817096+BenCos17@users.noreply.github.com>
2023-03-28 21:19:07 +0100

    update with levels

--- flight/flight.py
+++ flight/flight.py
@@ -1,5 +1,6 @@
 import discord
 from redbot.core import commands
+import random
 
 class RedFlight(commands.Cog):
     def __init__(self, bot):
@@ -7,6 +8,7 @@ class RedFlight(commands.Cog):
         self.aircraft = None
         self.level = None
         self.score = 0
+        self.obstacles = []
 
     @commands.command(name='start')
     async def start_game(self, ctx):
@@ -32,6 +34,33 @@ class RedFlight(commands.Cog):
             await ctx.send(f"You have chosen the {self.aircraft} aircraft.")
         except:
             await ctx.send("You did not choose an aircraft in time. Please try again with !aircraft.")
+            return
+
+        # Generate obstacles for each level
+        self.obstacles = [
+            ["Clouds", "Turbulence", "Birds"],
+            ["Storm clouds", "High winds", "Hail"],
+            ["Thunderstorms", "Lightning", "Ice"]
+        ]
+
+        # Start at level 1
+        self.level = 1
+        await self.start_level(ctx)
+
+    async def start_level(self, ctx):
+        await ctx.send(f"Starting level {self.level}! Good luck, pilot.")
+        await ctx.send("Here are the obstacles for this level:")
+        for obstacle in self.obstacles[self.level - 1]:
+            await ctx.send(f"- {obstacle}")
+
+    async def end_level(self, ctx):
+        await ctx.send("Congratulations! You have completed this level.")
+        self.score += 100 * self.level
+        if self.level < len(self.obstacles):
+            self.level += 1
+            await self.start_level(ctx)
+        else:
+            await ctx.send("You have completed all available levels. Final score: {self.score}")
 
     @commands.command(name='help')
     async def help_command(self, ctx):
