9bd791e8a8e77bb2d714c664f2638b143e45aff8
Ben Cos <52817096+BenCos17@users.noreply.github.com>
2023-04-01 18:54:09 +0100

    add april fools day prank

--- serverimage/serverimage.py
+++ serverimage/serverimage.py
@@ -2,6 +2,9 @@ import aiohttp
 from redbot.core import commands
 from io import BytesIO
 
+from PIL import Image, ImageDraw, ImageFont
+import urllib.request
+
 class ServerImageCog(commands.Cog):
     def __init__(self, bot):
         self.bot = bot
@@ -37,6 +40,32 @@ class ServerImageCog(commands.Cog):
                 await ctx.send('Server icon has been updated!')
             else:
                 await ctx.send('Error: Unsupported image type given.')
+    
+    @commands.command()
+    async def fakeping(self, ctx):
+        # Get the server icon URL
+        server = ctx.guild
+        icon_url = server.icon_url_as(format='png')
+        
+        # Open the icon image from URL
+        with urllib.request.urlopen(icon_url) as url:
+            icon_image = Image.open(url)
+            
+        # Create a red circle image with the same size as the icon
+        circle_image = Image.new('RGBA', icon_image.size, (0, 0, 0, 0))
+        draw = ImageDraw.Draw(circle_image)
+        draw.ellipse((0, 0, icon_image.size[0], icon_image.size[1]), fill=(255, 0, 0, 128))
+        
+        # Paste the icon on top of the circle
+        result_image = Image.alpha_composite(circle_image, icon_image)
+        
+        # Save the image to a file
+        result_bytes = BytesIO()
+        result_image.save(result_bytes, format='PNG')
+        result_bytes.seek(0)
+        
+        # Send the image as an attachment
+        await ctx.send(file=discord.File(result_bytes, 'fake_ping.png'))
 
 def setup(bot):
     bot.add_cog(ServerImageCog(bot))
