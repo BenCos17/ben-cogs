37ed4502c021347387b440517b98963a686165e6
Ben Cos <52817096+BenCos17@users.noreply.github.com>
2023-04-15 02:04:51 +0100

    add text and message forwarding support

--- userphone/userphone.py
+++ userphone/userphone.py
@@ -8,6 +8,7 @@ class UserPhone(commands.Cog):
         self.bot = bot
         self.calling_users = set()
         self.call_history = []
+        self.active_calls = {}
 
     @commands.command()
     async def call(self, ctx):
@@ -27,6 +28,8 @@ class UserPhone(commands.Cog):
             await ctx.send(f"{ctx.author.name} is calling {user_obj.name}...")
             self.calling_users.add(ctx.author.id)
             self.calling_users.add(user_obj.id)
+            self.active_calls[ctx.author.id] = user_obj.id
+            self.active_calls[user_obj.id] = ctx.author.id
         else:
             await ctx.send("Sorry, the chosen user cannot be reached at this time.")
         return
@@ -37,16 +40,28 @@ class UserPhone(commands.Cog):
             await ctx.send("You're not on a call right now!")
             return
 
-        user_id = next((u for u in self.calling_users if u != ctx.author.id), None)
-        if user_id is None:
-            await ctx.send("You're not on a call right now!")
-            return
-
+        user_id = self.active_calls[ctx.author.id]
         user_obj = await self.bot.fetch_user(user_id)
 
         self.calling_users.remove(ctx.author.id)
         self.calling_users.remove(user_id)
+        self.active_calls.pop(ctx.author.id)
+        self.active_calls.pop(user_id)
         await ctx.send(f"You hung up the call with {user_obj.name}.")
+
+    @commands.Cog.listener()
+    async def on_message(self, message):
+        if message.author.bot:
+            return
+        if message.author.id in self.active_calls:
+            recipient_id = self.active_calls[message.author.id]
+            recipient = await self.bot.fetch_user(recipient_id)
+            if recipient:
+                await recipient.send(f"{message.author.name}: {message.content}")
+            else:
+                await message.author.send("Sorry, the recipient cannot be reached at this time.")
+        elif message.content == "!endcall":
+            await self.hangup(message)
 
     @commands.Cog.listener()
     async def on_command_completion(self, ctx):
@@ -54,5 +69,23 @@ class UserPhone(commands.Cog):
             self.call_history.append((ctx.author.id, datetime.now()))
             self.call_history = [(u, t) for u, t in self.call_history if datetime.now() - t <= timedelta(seconds=5)]
 
-def setup(bot):
-    bot.add_cog(UserPhone(bot))
+        elif ctx.command.name == "hangup":
+            if ctx.author.id in self.active_calls:
+                await self.hangup(ctx)
+
+    async def timeout_call(self, user_id):
+        user_obj = await self.bot.fetch_user(user_id)
+        self.calling_users.remove(user_id)
+        await user_obj.send("Sorry, the call has timed out due to inactivity.")
+
+    async def start_timeout(self, user_id):
+        await discord.utils.sleep_until(datetime.now() + timedelta(seconds=30))
+        if user_id in self.calling_users:
+            await self.timeout_call(user_id)
+    
+    @commands.command()
+    async def endcall(self, ctx):
+        if ctx.author.id in self.active_calls:
+            await self.hangup(ctx)
+
+   
