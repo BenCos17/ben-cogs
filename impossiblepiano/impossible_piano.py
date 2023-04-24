148f76ed0100d8f4e7faa77f183a86ca84a6469b
Ben Cos <52817096+BenCos17@users.noreply.github.com>
2023-04-24 18:26:48 +0100

    bug fixes again lol

--- impossiblepiano/impossible_piano.py
+++ impossiblepiano/impossible_piano.py
@@ -14,6 +14,13 @@ class ImpossiblePiano(commands.Cog):
         octave = random.choice(octaves)
         return f"{note}{octave}"
 
+    def get_pitch(self, note):
+        notes = {"C": 60, "C#": 61, "D": 62, "D#": 63, "E": 64, "F": 65, "F#": 66, "G": 67, "G#": 68, "A": 69, "A#": 70, "B": 71}
+        octave = int(note[-1])
+        note_name = note[:-1]
+        pitch = notes[note_name] + (12 * (octave - 4))
+        return pitch
+
     def generate_melody(self, length):
         melody = []
         for i in range(length):
@@ -35,33 +42,25 @@ class ImpossiblePiano(commands.Cog):
             time += duration
         return midi
 
-    def get_pitch(self, note):
-        notes = {"C": 60, "C#": 61, "D": 62, "D#": 63, "E": 64, "F": 65, "F#": 66, "G": 67, "G#": 68, "A": 69, "A#": 70, "B": 71}
-        octave = int(note[-1])
-        note_name = note[:-1]
-        pitch = notes[note_name] + (12 * (octave - 4))
-        return pitch
+    @commands.command()
+    async def piano(self, ctx):
+        length = 20
+        melody = self.generate_melody(length)
+        midi_data = self.melody_to_midi(melody)
+        with open("impossible_piano.mid", "wb") as output_file:
+            midi_data.writeFile(output_file)
 
-@commands.command()
- async def piano(self, ctx):
-    length = 20
-    melody = self.generate_melody(length)
-    midi_data = self.melody_to_midi(melody)
-    with open("impossible_piano.mid", "wb") as output_file:
-        midi_data.writeFile(output_file)
+        voice_client = ctx.voice_client
+        if not voice_client:
+            if ctx.author.voice:
+                voice_client = await ctx.author.voice.channel.connect()
+            else:
+                await ctx.send("You must be in a voice channel to use this command.")
+                return
 
-    voice_client = ctx.voice_client
-    if not voice_client:
-        if ctx.author.voice:
-            voice_client = await ctx.author.voice.channel.connect()
-        else:
-            await ctx.send("You must be in a voice channel to use this command.")
-            return
+        if voice_client.is_playing():
+            voice_client.stop()
 
-    if voice_client.is_playing():
-        voice_client.stop()
-
-    source = discord.PCMVolumeTransformer(discord.FFmpegPCMAudio("impossible_piano.mid"))
-    voice_client.play(source)
-    await ctx.send("Playing impossible piano song.")
-
+        source = discord.PCMVolumeTransformer(discord.FFmpegPCMAudio("impossible_piano.mid"))
+        voice_client.play(source)
+        await ctx.send("Playing impossible piano song.")
