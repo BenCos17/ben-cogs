9d7bf75ab5b4b3fc265018a413fc0ae6cd857323
Ben Cos <52817096+BenCos17@users.noreply.github.com>
2023-06-13 19:57:06 +0100

    Update linklist.py

--- LinkList/linklist.py
+++ LinkList/linklist.py
@@ -4,106 +4,148 @@ from redbot.core import commands, Config
 class LinkList(commands.Cog):
     def __init__(self, bot):
         self.bot = bot
-        self.config = Config.get_conf(self, identifier=1234567890)  # use your own identifier here
-        default_global = {"link_links": {}, "protected_links": []}
+        self.config = Config.get_conf(self, identifier=1234567890, force_registration=True)
+        default_guild = {"link_links": {}, "protected_links": []}
+        default_global = {"global_links": {}}
+        self.config.register_guild(**default_guild)
         self.config.register_global(**default_global)
 
     async def cog_check(self, ctx):
-        # Restrict protected link commands to the bot owner
-        if ctx.invoked_with in ("addlink", "deletelink", "updatenitro") and not await self.bot.is_owner(ctx.author):
-            raise commands.NotOwner("You are not the bot owner.")
-        return True
+        # Allow bot owners to use global link commands
+        if await self.bot.is_owner(ctx.author):
+            return True
+        return False
 
     @commands.command()
     async def listlink(self, ctx, link_name: str):
-        link_links = await self.config.link_links()
-        if link_name not in link_links:
-            await ctx.send(f"Invalid link name '{link_name}'. Use `!addlink` to add a new Nitro link.")
+        guild_link_links = await self.config.guild(ctx.guild).link_links()
+        global_link_links = await self.config.global_links()
+        if link_name in guild_link_links:
+            await ctx.send(guild_link_links[link_name]["url"])
+        elif link_name in global_link_links:
+            await ctx.send(global_link_links[link_name]["url"])
         else:
-            await ctx.send(link_links[link_name]["url"])
+            await ctx.send(f"Invalid link name '{link_name}'. Use `!addlink` to add a new link.")
 
     @commands.command()
-    @commands.is_owner()
+    @commands.guild_only()
+    @commands.has_permissions(manage_guild=True)
     async def addlink(self, ctx, link_name: str, link_url: str):
-        link_links = await self.config.link_links()
-        if link_name in link_links:
+        guild_link_links = await self.config.guild(ctx.guild).link_links()
+        global_link_links = await self.config.global_links()
+        if link_name in guild_link_links or link_name in global_link_links:
             await ctx.send(f"A link with the name '{link_name}' already exists. Use `!updatenitro` to update the link.")
         else:
-            await self.config.link_links.set_raw(link_name, value={"url": link_url})
-            await ctx.send(f"Added new link '{link_name}': {link_url}")
+            await self.config.guild(ctx.guild).link_links.set_raw(link_name, value={"url": link_url})
+            await ctx.send(f"Added new guild-specific link '{link_name}': {link_url}")
 
     @commands.command()
     async def listlinks(self, ctx):
-        link_links = await self.config.link_links()
-        if not link_links:
+        guild_link_links = await self.config.guild(ctx.guild).link_links()
+        global_link_links = await self.config.global_links()
+        if not guild_link_links and not global_link_links:
             await ctx.send("There are no links configured.")
         else:
             embed = discord.Embed(title="Available Links", color=discord.Color.blue())
-            for name, link in link_links.items():
-                embed.add_field(name=name, value=link["url"], inline=False)
+            if guild_link_links:
+                for name, link in guild_link_links.items():
+                    embed.add_field(name=name, value=link["url"], inline=False)
+            if global_link_links:
+                for name, link in global_link_links.items():
+                    embed.add_field(name=name, value=link["url"], inline=False)
             await ctx.send(embed=embed)
 
     @commands.command()
-    @commands.is_owner()
+    @commands.guild_only()
+    @commands.has_permissions(manage_guild=True)
     async def deletelink(self, ctx, link_name: str):
-        link_links = await self.config.link_links()
-        protected_links = await self.config.protected_links()
-        if link_name not in link_links:
-            await ctx.send(f"Invalid link name '{link_name}'.")
-        elif link_name in protected_links:
-            await ctx.send(f"The link '{link_name}' is protected and cannot be deleted.")
+        guild_link_links = await self.config.guild(ctx.guild).link_links()
+        global_link_links = await self.config.global_links()
+        protected_links = await self.config.guild(ctx.guild).protected_links()
+        if link_name in guild_link_links:
+            if link_name in protected_links:
+                await ctx.send(f"The guild-specific link '{link_name}' is protected and cannot be deleted.")
+            else:
+                await self.config.guild(ctx.guild).link_links.clear_raw(link_name)
+                await ctx.send(f"Removed guild-specific link '{link_name}'.")
+        elif link_name in global_link_links:
+            await ctx.send(f"The global link '{link_name}' cannot be deleted.")
         else:
-            await self.config.link_links.clear_raw(link_name)
-            await ctx.send(f"Removed link '{link_name}'.")
+            await ctx.send(f"Invalid link name '{link_name}'.")
 
     @commands.command()
     @commands.is_owner()
-    async def updatelinks(self, ctx, link_name: str, link_url: str):
-        link_links = await self.config.link_links()
-        if link_name not in link_links:
-            await ctx.send(f"Invalid link name '{link_name}'. Use `!addlink` to add a new link.")
+    async def updatenitro(self, ctx, link_name: str, link_url: str):
+        guild_link_links = await self.config.guild(ctx.guild).link_links()
+        global_link_links = await self.config.global_links()
+        if link_name in guild_link_links:
+            await self.config.guild(ctx.guild).link_links.set_raw(link_name, value={"url": link_url})
+            await ctx.send(f"Updated guild-specific link '{link_name}': {link_url}")
+        elif link_name in global_link_links:
+            await self.config.global_links.set_raw(link_name, value={"url": link_url})
+            await ctx.send(f"Updated global link '{link_name}': {link_url}")
         else:
-            await self.config.link_links.set_raw(link_name, value={"url": link_url})
-            await ctx.send(f"Updated link '{link_name}': {link_url}")
+            await ctx.send(f"Invalid link name '{link_name}'. Use `!addlink` to add a new link.")
 
     @commands.command()
     @commands.is_owner()
     async def linkcount(self, ctx):
-        link_links = await self.config.link_links()
-        if not link_links:
-            await ctx.send("There are no links configured.")
+        global_link_links = await self.config.global_links()
+        if not global_link_links:
+            await ctx.send("There are no global links configured.")
         else:
-            counts = {name: 0 for name in link_links.keys()}
+            counts = {name: 0 for name in global_link_links.keys()}
             messages = await ctx.channel.history(limit=1000).flatten()
             for message in messages:
                 for word in message.content.split():
-                    if word in link_links:
+                    if word in global_link_links:
                         counts[word] += 1
             count_list = "\n".join([f"{name}: {count}" for name, count in counts.items()])
             await ctx.send(f"Usage counts:\n{count_list}")
 
     @commands.command()
     @commands.is_owner()
-    async def protectlink(self, ctx, link_name: str):
-        protected_links = await self.config.protected_links()
-        link_links = await self.config.link_links()
-        if link_name not in link_links:
-            await ctx.send(f"Invalid link name '{link_name}'.")
-        elif link_name in protected_links:
-            await ctx.send(f"The link '{link_name}' is already protected.")
+    async def addgloballink(self, ctx, link_name: str, link_url: str):
+        global_link_links = await self.config.global_links()
+        if link_name in global_link_links:
+            await ctx.send(f"A global link with the name '{link_name}' already exists. Use `!updatenitro` to update the link.")
         else:
+            await self.config.global_links.set_raw(link_name, value={"url": link_url})
+            await ctx.send(f"Added new global link '{link_name}': {link_url}")
+
+    @commands.command()
+    @commands.is_owner()
+    async def deletegloballink(self, ctx, link_name: str):
+        global_link_links = await self.config.global_links()
+        if link_name not in global_link_links:
+            await ctx.send(f"Invalid global link name '{link_name}'.")
+        else:
+            await self.config.global_links.clear_raw(link_name)
+            await ctx.send(f"Removed global link '{link_name}'.")
+
+    @commands.command()
+    @commands.is_owner()
+    async def protectlink(self, ctx, link_name: str):
+        protected_links = await self.config.guild(ctx.guild).protected_links()
+        guild_link_links = await self.config.guild(ctx.guild).link_links()
+        global_link_links = await self.config.global_links()
+        if link_name in guild_link_links and link_name not in protected_links:
             protected_links.append(link_name)
-            await self.config.protected_links.set(protected_links)
-            await ctx.send(f"The link '{link_name}' is now protected and cannot be deleted or edited.")
+            await self.config.guild(ctx.guild).protected_links.set(protected_links)
+            await ctx.send(f"The guild-specific link '{link_name}' is now protected and cannot be deleted or edited.")
+        elif link_name in global_link_links:
+            await ctx.send(f"The global link '{link_name}' cannot be protected.")
+        else:
+            await ctx.send(f"Invalid link name '{link_name}'.")
 
     @commands.command()
     @commands.is_owner()
     async def unprotectlink(self, ctx, link_name: str):
-        protected_links = await self.config.protected_links()
-        if link_name not in protected_links:
-            await ctx.send(f"The link '{link_name}' is not protected.")
-        else:
+        protected_links = await self.config.guild(ctx.guild).protected_links()
+        if link_name in protected_links:
             protected_links.remove(link_name)
-            await self.config.protected_links.set(protected_links)
-            await ctx.send(f"The link '{link_name}' is no longer protected.")
+            await self.config.guild(ctx.guild).protected_links.set(protected_links)
+            await ctx.send(f"The guild-specific link '{link_name}' is no longer protected.")
+        else:
+            await ctx.send(f"The guild-specific link '{link_name}' is not protected.")
 
