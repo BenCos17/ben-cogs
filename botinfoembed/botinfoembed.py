ef96f755b65fc9b5d9812ad491a7e343004ea471
Ben Cos <52817096+BenCos17@users.noreply.github.com>
2023-06-28 16:39:03 +0100

    Update botinfoembed.py

--- botinfoembed/botinfoembed.py
+++ botinfoembed/botinfoembed.py
@@ -1,6 +1,7 @@
 from redbot.core import commands, Config
+import discord
 
-class BotSettings(commands.Cog):
+class BotInfoEmbed(commands.Cog):
     def __init__(self, bot):
         self.bot = bot
         self.config = Config.get_conf(self, identifier=1234567890)  # Use a unique identifier for your cog
