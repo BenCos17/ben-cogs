d1ba54fc36d119ce4b8424a8bbd3f47b4350e896
Ben Cos <52817096+BenCos17@users.noreply.github.com>
2023-06-30 21:20:32 +0100

    Update invitecog.py

--- createinvite/invitecog.py
+++ createinvite/invitecog.py
@@ -7,9 +7,17 @@ class InviteCog(commands.Cog):
 
     @commands.command()
     @commands.has_permissions(administrator=True)
-    async def createinvite(self, ctx, max_uses=1, unique=True):
-        invite = await ctx.channel.create_invite(max_uses=max_uses, unique=unique)
-        await ctx.send(f"Invite created: {invite.url}")
+    async def createinvite(self, ctx, max_uses=1, unique=True, channel=None):
+        if not channel:
+            channel = ctx.channel
+        else:
+            channel = discord.utils.get(ctx.guild.channels, name=channel)
+        if not channel:
+            await ctx.send("Invalid channel name.")
+            return
+        
+        invite = await channel.create_invite(max_uses=max_uses, unique=unique)
+        await ctx.send(f"Invite created in {channel.mention}: {invite.url}")
 
 def setup(bot):
     cog = InviteCog(bot)
