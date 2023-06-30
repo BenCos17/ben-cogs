f3295985c58fd48990ced74ec83f84762eb79c2b
Ben Cos <52817096+BenCos17@users.noreply.github.com>
2023-06-30 22:38:22 +0100

    Update roast.py

--- roast/roast.py
+++ roast/roast.py
@@ -5,7 +5,7 @@ import random
 import os
 import requests
 
-class Roast(commands.Cog):
+class RoastCog(commands.Cog):
     def __init__(self, bot):
         self.bot = bot
         self.roasts = self.load_roasts()
