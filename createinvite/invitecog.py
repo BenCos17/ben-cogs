082c6fe9e713ad4df50080bd0353a71f4cad59e6
Ben Cos <52817096+BenCos17@users.noreply.github.com>
2023-06-30 21:24:16 +0100

    Update invitecog.py

--- createinvite/invitecog.py
+++ createinvite/invitecog.py
@@ -15,9 +15,14 @@ class InviteCog(commands.Cog):
         if not channel:
             await ctx.send("Invalid channel name.")
             return
-        
+
+        if max_uses < 0 or max_uses > 100:
+            await ctx.send("The maximum uses must be between 0 and 100.")
+            return
+
         invite = await channel.create_invite(max_uses=max_uses, unique=unique)
-        await ctx.send(f"Invite created in {channel.mention}: {invite.url}")
+        uses_str = "infinite" if max_uses == 0 else str(max_uses)
+        await ctx.send(f"Invite created in {channel.mention} with {uses_str} uses: {invite.url}")
 
 def setup(bot):
     cog = InviteCog(bot)
