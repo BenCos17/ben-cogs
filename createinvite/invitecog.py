976393a7f8d94c86692357fd16a7f43d71f402ca
Ben Cos <52817096+BenCos17@users.noreply.github.com>
2023-06-30 21:26:37 +0100

    Update invitecog.py

--- createinvite/invitecog.py
+++ createinvite/invitecog.py
@@ -7,7 +7,7 @@ class InviteCog(commands.Cog):
 
     @commands.command()
     @commands.has_permissions(administrator=True)
-    async def createinvite(self, ctx, max_uses=1, unique=True, channel=None):
+    async def createinvite(self, ctx, max_uses=1, unique=True, channel=None, reuse_existing=True):
         if not channel:
             channel = ctx.channel
         else:
@@ -20,9 +20,25 @@ class InviteCog(commands.Cog):
             await ctx.send("The maximum uses must be between 0 and 100.")
             return
 
-        invite = await channel.create_invite(max_uses=max_uses, unique=unique)
+        if reuse_existing:
+            existing_invite = await self.find_existing_invite(channel)
+            if existing_invite:
+                invite = existing_invite
+            else:
+                invite = await channel.create_invite(max_uses=max_uses, unique=unique)
+        else:
+            invite = await channel.create_invite(max_uses=max_uses, unique=unique)
+        
         uses_str = "infinite" if max_uses == 0 else str(max_uses)
-        await ctx.send(f"Invite created in {channel.mention} with {uses_str} uses: {invite.url}")
+        action_str = "Updated existing invite" if reuse_existing and existing_invite else "Created new invite"
+        await ctx.send(f"{action_str} in {channel.mention} with {uses_str} uses: {invite.url}")
+
+    async def find_existing_invite(self, channel):
+        invites = await channel.invites()
+        for invite in invites:
+            if invite.inviter == self.bot.user:
+                return invite
+        return None
 
 def setup(bot):
     cog = InviteCog(bot)
