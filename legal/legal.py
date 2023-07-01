52935017c2f31104ecbb5fd182e9cdd9ef0d6483
Ben Cos <52817096+BenCos17@users.noreply.github.com>
2023-07-01 12:27:36 +0100

    Update legal.py

--- legal/legal.py
+++ legal/legal.py
@@ -5,59 +5,79 @@ import asyncio
 class Legal(commands.Cog):
     def __init__(self, bot):
         self.bot = bot
+        self.players = {}  # Dictionary to store player-role mappings
 
     @commands.command()
     async def trial(self, ctx):
         await ctx.send("Welcome to the legal trial simulation!")
 
-        # Define the roles allowed to participate in the trial
-        allowed_roles = ["Prosecution", "Defense"]
+        # Define the names of the different roles
+        role_names = ["Judge", "Prosecution", "Defense", "Witness", "Jury"]
 
-        # Check if the command author has any of the allowed roles
-        if not any(role.name in allowed_roles for role in ctx.author.roles):
-            await ctx.send("You don't have the required role to participate in the trial.")
-            return
+        # Prompt users to choose their roles
+        for i in range(len(role_names)):
+            await ctx.send(f"{ctx.author.mention}, please choose your desired role: {role_names}")
+            role = await self.await_user_role(ctx, role_names)
+            self.players[ctx.author.id] = role
+
+            role_names.remove(role)
+            await ctx.send(f"{ctx.author.mention}, you have chosen the role: {role}")
 
         # Opening statements
         await ctx.send("The trial is now in session.")
-        await ctx.send("The prosecution may present their opening statement.")
+        await ctx.send(f"{self.get_role_mention('Judge')}, please present the opening statement.")
         await self.await_user_response(ctx)
 
-        await ctx.send("The defense may present their opening statement.")
+        await ctx.send(f"{self.get_role_mention('Prosecution')}, please present the opening statement.")
         await self.await_user_response(ctx)
 
         # Witness testimonies
-        await ctx.send("The prosecution may call their first witness.")
+        await ctx.send(f"{self.get_role_mention('Prosecution')}, please call your first witness.")
         await self.await_user_response(ctx)
 
-        await ctx.send("The defense may cross-examine the witness.")
+        await ctx.send(f"{self.get_role_mention('Defense')}, you may cross-examine the witness.")
         await self.await_user_response(ctx)
 
-        await ctx.send("The defense may call their first witness.")
+        await ctx.send(f"{self.get_role_mention('Defense')}, please call your first witness.")
         await self.await_user_response(ctx)
 
-        await ctx.send("The prosecution may cross-examine the witness.")
+        await ctx.send(f"{self.get_role_mention('Prosecution')}, you may cross-examine the witness.")
         await self.await_user_response(ctx)
 
         # Presentation of evidence
-        await ctx.send("The prosecution may present their evidence.")
+        await ctx.send(f"{self.get_role_mention('Prosecution')}, please present your evidence.")
         await self.await_user_response(ctx)
 
-        await ctx.send("The defense may present their evidence.")
+        await ctx.send(f"{self.get_role_mention('Defense')}, please present your evidence.")
         await self.await_user_response(ctx)
 
         # Closing arguments
-        await ctx.send("The prosecution may present their closing argument.")
+        await ctx.send(f"{self.get_role_mention('Prosecution')}, please present your closing argument.")
         await self.await_user_response(ctx)
 
-        await ctx.send("The defense may present their closing argument.")
+        await ctx.send(f"{self.get_role_mention('Defense')}, please present your closing argument.")
         await self.await_user_response(ctx)
 
         # Verdict
-        await ctx.send("The trial is now complete. The judge will deliver the verdict.")
+        await ctx.send(f"{self.get_role_mention('Judge')}, please deliver the verdict.")
         await self.await_user_response(ctx)
 
         await ctx.send("Thank you for participating in the legal trial simulation.")
+
+    async def await_user_role(self, ctx, role_names):
+        try:
+            def check(m):
+                return (
+                    m.author == ctx.author
+                    and m.channel == ctx.channel
+                    and m.content in role_names
+                )
+
+            msg = await self.bot.wait_for('message', check=check, timeout=300)
+            return msg.content
+        except asyncio.TimeoutError:
+            await ctx.send("Timeout: No response received. Ending trial simulation.")
+            raise commands.CommandError("Simulation timeout.")
 
     async def await_user_response(self, ctx):
         try:
@@ -69,5 +89,11 @@ class Legal(commands.Cog):
             await ctx.send("Timeout: No response received. Ending trial simulation.")
             raise commands.CommandError("Simulation timeout.")
 
+    def get_role_mention(self, role_name):
+        role_id = next((k for k, v in self.players.items() if v == role_name), None)
+        if role_id:
+            return f"<@{role_id}>"
+        return ""
+
 def setup(bot):
-    bot.add_cog(LegalCog(bot))
+    bot.add_cog(Legal(bot))
