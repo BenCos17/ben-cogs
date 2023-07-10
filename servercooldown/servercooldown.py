737b8e69b8204c03474fb1e481a556467bba28d8
Ben Cos <52817096+BenCos17@users.noreply.github.com>
2023-07-10 15:48:43 +0100

    Update servercooldown.py

--- servercooldown/servercooldown.py
+++ servercooldown/servercooldown.py
@@ -2,7 +2,7 @@ from redbot.core import commands
 from redbot.core import Config
 from redbot.core.bot import Red
 
-class ServerCooldown(commands.Cog):
+class Cooldown(commands.Cog):
     def __init__(self, bot: Red):
         self.bot = bot
         self.config = Config.get_conf(self, identifier=1234567890)  # Use a unique identifier
@@ -18,11 +18,8 @@ class ServerCooldown(commands.Cog):
     async def setcooldown(self, ctx: commands.Context, command_name: str, cooldown_time: int):
         """
         Set the cooldown for a specific command on the server.
-        Usage: !setcooldown <command_name> <cooldown_time>
+        Usage: [p]setservercooldown <command_name> <cooldown_time>
         """
-        if not ctx.guild:
-            return
-
         if cooldown_time < 0:
             await ctx.send("Cooldown time cannot be negative.")
             return
@@ -40,11 +37,8 @@ class ServerCooldown(commands.Cog):
     async def servercooldown(self, ctx: commands.Context, command_name: str):
         """
         Get the current cooldown for a specific command on the server.
-        Usage: !servercooldown <command_name>
+        Usage: [p]servercooldown <command_name>
         """
-        if not ctx.guild:
-            return
-
         command_name = command_name.lower()
         command_cooldowns = await self.config.guild(ctx.guild).command_cooldowns()
 
@@ -56,16 +50,14 @@ class ServerCooldown(commands.Cog):
 
     @commands.Cog.listener()
     async def on_command(self, ctx: commands.Context):
-        if not ctx.guild:
-            return
+        if ctx.guild:
+            command_name = ctx.command.qualified_name.lower()
+            command_cooldowns = await self.config.guild(ctx.guild).command_cooldowns()
 
-        command_name = ctx.command.qualified_name.lower()
-        command_cooldowns = await self.config.guild(ctx.guild).command_cooldowns()
-
-        if command_name in command_cooldowns:
-            cooldown_time = command_cooldowns[command_name]
-            await ctx.command.reset_cooldown(ctx)
-            ctx.command._buckets._cooldown = commands.Cooldown(rate=1, per=cooldown_time)
+            if command_name in command_cooldowns:
+                cooldown_time = command_cooldowns[command_name]
+                await ctx.command.reset_cooldown(ctx)
+                ctx.command._buckets._cooldown = commands.Cooldown(rate=1, per=cooldown_time)
 
 def setup(bot: Red):
     bot.add_cog(Cooldown(bot))
