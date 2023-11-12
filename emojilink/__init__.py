afb541298d9ae30c68e71524617e9e76b6ef8159
Ben Cos <52817096+BenCos17@users.noreply.github.com>
2023-11-12 22:21:30 +0000

    Update __init__.py

--- emojilink/__init__.py
+++ emojilink/__init__.py
@@ -27,9 +27,5 @@ from redbot.core.bot import Red
 
 from .emojilink import EmojiLink
 
-with open(Path(__file__).parent / "info.json") as fp:
-    __red_end_user_data_statement__ = json.load(fp)["end_user_data_statement"]
-
-
 async def setup(bot: Red):
     await bot.add_cog(EmojiLink(bot))
