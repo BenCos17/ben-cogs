7d5340726b51cd6d0ebcd95133fcd8c39c4b9bea
Ben Cos <52817096+BenCos17@users.noreply.github.com>
2023-12-15 19:57:14 +0000

    Update spamatron.py

--- spamatron/spamatron.py
+++ spamatron/spamatron.py
@@ -19,11 +19,30 @@ class Spamatron(commands.Cog):
         """Spam a message in a channel a specified number of times."""
         target_channel = await self.parse_channel_mention(ctx, channel_mention)
         if not target_channel:
-            await ctx.send("Invalid channel mention.")
-            return
+            return await ctx.send("Invalid channel mention.")
+
+        if amount <= 0:
+            return await ctx.send("Please provide a positive number for the amount.")
+
+        confirmation = await ctx.send(
+            f"Are you sure you want to send `{amount}` messages to {target_channel.mention}? Reply with `yes` to confirm."
+        )
+
+        try:
+            reply = await self.bot.wait_for(
+                "message",
+                check=lambda m: m.author == ctx.author
+                and m.channel == ctx.channel
+                and m.content.lower() == "yes",
+                timeout=30,
+            )
+        except TimeoutError:
+            return await ctx.send("Confirmation timed out. Aborting.")
 
         for _ in range(amount):
             await target_channel.send(message)
+
+        await ctx.send(f"Successfully sent `{amount}` messages to {target_channel.mention}.")
 
 def setup(bot):
     bot.add_cog(Spamatron(bot))
