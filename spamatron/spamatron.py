88ae0c674641bdca7615919685ac20709bf5547b
Ben Cos <52817096+BenCos17@users.noreply.github.com>
2023-12-15 19:42:59 +0000

    Update spamatron.py

--- spamatron/spamatron.py
+++ spamatron/spamatron.py
@@ -16,21 +16,17 @@ class Spamatron(commands.Cog):
     @commands.command()
     @commands.has_permissions(administrator=True)
     async def spam(self, ctx, channel_mention: str, *, args: str):
-        """Spam a message in a channel a specified number of times."""
+        """Spam a message in a channel."""
         try:
             amount, message = args.split(maxsplit=1)
             amount = int(amount)
         except ValueError:
-            await ctx.send("Please provide the command in the format: `spam #channel message amount`")
+            await ctx.send("Please provide the command in the format: `spam #channel message`")
             return
 
         target_channel = await self.parse_channel_mention(ctx, channel_mention)
         if not target_channel:
             await ctx.send("Invalid channel mention.")
-            return
-
-        if amount > 10:
-            await ctx.send("Please limit the amount of spam to 10 or fewer for safety reasons.")
             return
 
         for _ in range(amount):
