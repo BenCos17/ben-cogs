ab89eedcb5c806347824874b97e15e9c9cafc080
Ben Cos <52817096+BenCos17@users.noreply.github.com>
2023-12-15 19:39:40 +0000

    Update spamatron.py

--- spamatron/spamatron.py
+++ spamatron/spamatron.py
@@ -5,19 +5,26 @@ class Spamatron(commands.Cog):
     def __init__(self, bot):
         self.bot = bot
 
+    async def parse_channel_mention(self, ctx, channel_mention):
+        if not channel_mention.startswith("<#") or not channel_mention.endswith(">"):
+            return None
+
+        channel_id = int(channel_mention[2:-1])
+        return ctx.guild.get_channel(channel_id)
+
     @commands.guild_only()
     @commands.command()
     @commands.has_permissions(administrator=True)
-    async def spam(self, ctx, channel: discord.TextChannel, *, message: str):
+    async def spam(self, ctx, channel_mention: str, *, args: str):
         """Spam a message in a channel a specified number of times."""
         try:
-            channel_mention, amount = message.split(maxsplit=1)
+            amount, message = args.split(maxsplit=1)
             amount = int(amount)
         except ValueError:
             await ctx.send("Please provide the command in the format: `spam #channel message amount`")
             return
 
-        target_channel = discord.utils.get(ctx.guild.channels, mention=channel_mention)
+        target_channel = await self.parse_channel_mention(ctx, channel_mention)
         if not target_channel:
             await ctx.send("Invalid channel mention.")
             return
