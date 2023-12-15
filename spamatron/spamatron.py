dd2e8bffdd33cddb3130adbcb9dae6bcbc3041f0
Ben Cos <52817096+BenCos17@users.noreply.github.com>
2023-12-15 19:17:18 +0000

    add file

--- spamatron/spamatron.py
+++ spamatron/spamatron.py
@@ -1,19 +1,21 @@
 from redbot.core import commands
 import discord
 
-class SpamPing(commands.Cog):
+class Spamatron(commands.Cog):
     def __init__(self, bot):
         self.bot = bot
 
+    @commands.guild_only()
     @commands.command()
-    async def spamping(self, ctx, user: discord.User, times: int = 5):
-        """Spam pings a user a specified number of times."""
-        if times > 10:
-            await ctx.send("Please limit the number of pings to 10 or fewer for spamming.")
+    @commands.has_permissions(administrator=True)
+    async def spam(self, ctx, channel: discord.TextChannel, *, message: str, amount: int):
+        """Spam a message in a channel a specified number of times."""
+        if amount > 10:
+            await ctx.send("Please limit the amount of spam to 10 or fewer for safety reasons.")
             return
 
-        for i in range(times):
-            await ctx.send(f"{user.mention}")
+        for _ in range(amount):
+            await channel.send(message)
 
 def setup(bot):
-    bot.add_cog(SpamPing(bot))
+    bot.add_cog(Spam(bot))
