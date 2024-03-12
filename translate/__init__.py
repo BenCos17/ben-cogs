46d6335f970414af1315e777452607e9ef16351a
Ben Cos <52817096+BenCos17@users.noreply.github.com>
2024-03-12 21:48:37 +0000

    remove old stuff

--- translate/__init__.py
+++
@@ -1,4 +0,0 @@
-from .translate import TranslateCog
-
-def setup(bot):
-    bot.add_cog(TranslateCog(bot))
--- userphone/__init__.py
+++
@@ -1,8 +0,0 @@
-from redbot.core import commands
-
-from .userphone import UserPhone
-
-
-def setup(bot: commands.Bot):
-    cog = UserPhone(bot)
-    bot.add_cog(cog)
