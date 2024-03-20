78d6fb9a7036fe51c9b65908ef23692d6cdef846
Ben Cos <52817096+BenCos17@users.noreply.github.com>
2024-03-20 21:52:35 +0000

    Update airplaneslive.py

--- airplaneslive/airplaneslive.py
+++ airplaneslive/airplaneslive.py
@@ -29,7 +29,7 @@ class Airplaneslive(commands.Cog):
         embed = discord.Embed(title='Aircraft Information', description=formatted_response, color=self.EMBED_COLOR)
         if image_url:
           #  embed.set_image(url=image_url)
-            embed.set_footer(text=f"Photograph by {photographer} | Powered by Planespotters.net and airplanes.live ✈️ \n{image_url}")
+            embed.set_footer(text=f"Powered by airplanes.live✈️")
         await ctx.send(embed=embed)
 
     def _format_response(self, response):
