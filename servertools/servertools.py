90a84dd690fa5d1bcd8abc7d723478ca11cf37ec
Ben Cos <52817096+BenCos17@users.noreply.github.com>
2024-06-23 21:37:53 +0100

    Update servertools.py

--- servertools/servertools.py
+++ servertools/servertools.py
@@ -10,10 +10,13 @@ class Servertools(commands.Cog):
     @commands.has_permissions(manage_guild=True)
     async def moddm(self, ctx, user: discord.User, *, message):
         if ctx.guild:
-            try:
-                await user.send(message)
-                await ctx.send(f"Message sent to {user.name}")
-            except discord.Forbidden:
-                await ctx.send("I cannot send a message to this user.")
+            if ctx.guild.get_member(user.id):
+                try:
+                    await user.send(message)
+                    await ctx.send(f"Message sent to {user.name}")
+                except discord.Forbidden:
+                    await ctx.send("I cannot send a message to this user.")
+            else:
+                await ctx.send("This user is not a member of this server.")
         else:
             await ctx.send("This command can only be used in a server.")
