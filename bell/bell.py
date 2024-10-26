28eee8230f98bd145007073b66777cc8dac5585a
Ben Cos <52817096+BenCos17@users.noreply.github.com>
2024-10-26 13:29:43 +0100

    Update bell.py

--- bell/bell.py
+++ bell/bell.py
@@ -37,7 +37,7 @@ class BellCog(commands.Cog):
         message += f"You have now rung the bell {user_bell_count} times in this server. 🔔"
 
         gif_url = "https://github.com/BenCos17/ben-cogs/blob/main/bell/bell.gif?raw=true"
-        await ctx.send(message + f"\n[Click here to see the GIF]({gif_url})")  # Include the GIF URL as a clickable link
+        await ctx.send(message + f"\n[gif]({gif_url})")  # Include the GIF URL as a clickable link
 
     @commands.command(aliases=['resetbell'])  
     async def reset_bell(self, ctx):
