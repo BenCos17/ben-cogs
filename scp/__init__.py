3fe7b2373cb2b2a1a71bd1d03d33287dce914ce0
Ben Cos <52817096+BenCos17@users.noreply.github.com>
2024-11-10 00:38:21 +0000

    init.py fix

--- scp/__init__.py
+++ scp/__init__.py
@@ -1,12 +1,6 @@
 from redbot.core.bot import Red
-from .scp import SCPLookup
 
-def setup(bot):
-    async def setup(bot: Red):
+from .scp import ScpLookup
 
-
-
-
-
-
-
+async def setup(bot: Red):
+    await bot.add_cog(ScpLookup(bot))
