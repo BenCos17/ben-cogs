94df732c4eb53241af9170c84aeeccfde5422d13
Ben Cos <52817096+BenCos17@users.noreply.github.com>
2025-07-18 22:15:16 +0100

    Update __init__.py

--- facebookdownloader/__init__.py
+++ facebookdownloader/__init__.py
@@ -1,4 +1,4 @@
 from .facebook_video_downloader import FacebookVideoDownloader
 
-def setup(bot):
-    bot.add_cog(FacebookVideoDownloader(bot)) 
+async def setup(bot):
+    await bot.add_cog(FacebookVideoDownloader(bot)) 
