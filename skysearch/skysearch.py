ec5af6000ed1eb4e539578ff5b0dd04e9e542bbb
BenCos17 <mail@jarvisdiscordbot.net>
2025-10-19 18:23:28 +0100

    Update skysearch.py

--- skysearch/skysearch.py
+++ skysearch/skysearch.py
@@ -655,11 +655,19 @@ class Skysearch(commands.Cog, DashboardIntegration):
                                         log.warning(f"Pre-send callback removed view for {icao_hex}, restoring buttons")
                                         message_data['view'] = original_view
 
-                                    # Send the message using the possibly modified data
+                                    # Send the message using the possibly modified data (allow role mentions)
+                                    allowed_mentions = None
+                                    if alert_role_id:
+                                        role_obj = guild.get_role(alert_role_id)
+                                        if role_obj:
+                                            allowed_mentions = discord.AllowedMentions(roles=[role_obj])
+                                        else:
+                                            allowed_mentions = discord.AllowedMentions(roles=True)
                                     sent_message = await alert_channel.send(
                                         content=message_data.get('content'),
                                         embed=message_data.get('embed'),
-                                        view=message_data.get('view')
+                                        view=message_data.get('view'),
+                                        allowed_mentions=allowed_mentions
                                     )
 
                                     # Let other cogs react after the message is sent
@@ -1098,11 +1106,19 @@ class Skysearch(commands.Cog, DashboardIntegration):
             log.warning(f"Pre-send callback removed view for {hex_code}, restoring buttons")
             message_data['view'] = original_view
 
-        # Send the message using the possibly modified data
+        # Send the message using the possibly modified data (allow role mentions)
+        allowed_mentions = None
+        if alert_role_id:
+            role_obj = guild.get_role(alert_role_id)
+            if role_obj:
+                allowed_mentions = discord.AllowedMentions(roles=[role_obj])
+            else:
+                allowed_mentions = discord.AllowedMentions(roles=True)
         sent_message = await alert_channel.send(
             content=message_data.get('content'),
             embed=message_data.get('embed'),
-            view=message_data.get('view')
+            view=message_data.get('view'),
+            allowed_mentions=allowed_mentions
         )
 
         # Let other cogs react after the message is sent
