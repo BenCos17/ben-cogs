cf032a031756b20a455a67ae14a09d500b17586b
Ben Cos <52817096+BenCos17@users.noreply.github.com>
2026-01-05 21:44:01 +0000

    repo cleanup
    clean up old cogs that are non functional/half-finished or just a bad idea all round

--- earthquake/__init__.py
+++
@@ -1,6 +0,0 @@
-from .earthquake import Earthquake
-
-__red_end_user_data_statement__ = "This cog does not store any end user data."
-
-async def setup(bot):
-    await bot.add_cog(Earthquake(bot))
--- imgen/__init__.py
+++
@@ -1,8 +0,0 @@
-from redbot.core.bot import Red
-
-from .Imgen import Imgen
-
-__red_end_user_data_statement__ = "This cog does not store any end user data."
-
-async def setup(bot: Red):
-    await bot.add_cog(Imgen(bot))
