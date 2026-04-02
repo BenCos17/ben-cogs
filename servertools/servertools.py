8364e261815508227da668b54d2ac3f484577437
Ben Cos <52817096+BenCos17@users.noreply.github.com>
2026-04-02 14:10:22 +0100

    fix fakeping

--- servertools/servertools.py
+++ servertools/servertools.py
@@ -246,7 +246,12 @@ class Servertools(commands.Cog):
                 except Exception:
                     font = ImageFont.load_default()
                 text = "1"
-                text_width, text_height = draw.textsize(text, font=font)
+                try:
+                    bbox = draw.textbbox((0, 0), text, font=font)
+                    text_width = bbox[2] - bbox[0]
+                    text_height = bbox[3] - bbox[1]
+                except Exception:
+                    text_width, text_height = font.getmask(text).size
                 text_x = badge_center[0] - text_width // 2
                 text_y = badge_center[1] - text_height // 2
                 draw.text((text_x, text_y), text, font=font, fill=(255, 255, 255, 255))
